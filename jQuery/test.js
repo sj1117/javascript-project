f9ee9dded1cb32b6c3d34c16247f9f9916a1e16d
sj1117 <kg0489@gmail.com>
2023-04-14 15:50:08 +0900

    up

---
+++ jQuery/test.js
@@ -0,0 +1,10 @@
+    (function(){
+        console.log("안녕하세요");
+        console.log("외부 파일로 실행");
+
+        function test () {
+            console.log("외부 파일로 실행")
+        }
+        
+        test();
+    })()
